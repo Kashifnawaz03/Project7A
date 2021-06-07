e561d8734a6be4c0e4a72806b2b5e14654c06d96
kashifnawaz <kashi030@hotmail.com>
2021-06-07 16:55:37 +0500

    Still

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-//import logo from './logo.svg';
+import {initNotificaton} from './services/firebaseService';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
      <h1>Hello world</h1>
+     <button onClick={initNotificaton}>Configure Notification</button>
     </div>
   );
 }
